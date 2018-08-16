f5a6960e99f0c92a22ac6f62fa661964cdc146a3
t-xinlin <t_xinlin@sina.com>
2018-08-16 17:23:56 +0800

    Create context.go

---
+++ test/context/context.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+)
+
+var logg *log.Logger
+
+func someHandler() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go doStuff(ctx)
+	go doStuff1(ctx)
+	//10秒后取消doStuff
+	time.Sleep(10 * time.Second)
+	cancel()
+
+}
+
+//每1秒work一下，同时会判断ctx是否被取消了，如果是就退出
+func doStuff(ctx context.Context) {
+	for {
+		time.Sleep(1 * time.Second)
+		select {
+		case <-ctx.Done():
+			logg.Printf("done")
+			return
+		default:
+			logg.Printf("work")
+		}
+	}
+}
+
+//每1秒work一下，同时会判断ctx是否被取消了，如果是就退出
+func doStuff1(ctx context.Context) {
+	for {
+		time.Sleep(1 * time.Second)
+		select {
+		case <-ctx.Done():
+			logg.Printf("done1")
+			return
+		default:
+			logg.Printf("work1")
+		}
+	}
+}
+
+func main() {
+	logg = log.New(os.Stdout, "", log.Ltime)
+	someHandler()
+	logg.Printf("down")
+}
