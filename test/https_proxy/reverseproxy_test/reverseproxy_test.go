a978306731bbaf590384f2418cc44715e978a2d2
t-xinlin <t_xinlin@sina.com>
2018-10-08 15:57:26 +0800

    Create reverseproxy_test.go

---
+++ test/https_proxy/reverseproxy_test/reverseproxy_test.go
@@ -0,0 +1,25 @@
+package reverseproxy_test
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/url"
+	"net/http"
+	"net/http/httputil"
+)
+
+func TestRP(t *testing.T) {
+	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { t.Log("headers: ", r.Header) }))
+	target, _ := url.Parse(backendServer.URL)
+	reverseProxy := httputil.NewSingleHostReverseProxy(target)
+	proxyServer := httptest.NewServer(reverseProxy)
+	req, err := http.NewRequest("POST", proxyServer.URL+"/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "abcdefg")
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
