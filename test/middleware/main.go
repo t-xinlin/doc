7fa80b5f3e97df9194e24dacd53c9226a69f0e8b
t-xinlin <t_xinlin@sina.com>
2019-02-13 18:06:07 +0800

    modify

--- test/middleware/main.go
+++ test/middleware/main.go
@@ -23,8 +23,43 @@ func timeMiddleware(next http.Handler) http.Handler {
 }
 
 func main() {
+	r := NewRouter()
+	//r.Use(logger)
+	//r.Use(timeout)
+	//r.Use(ratelimit)
+	//r.Add("/", http.HandlerFunc(hello))
 	http.Handle("/", timeMiddleware(http.HandlerFunc(hello)))
+	for k,v := range r.mux{
+		http.Handle(k, v)
+	}
 	if err := http.ListenAndServe(":8080", nil); err != nil{
 
 	}
+}
+
+
+
+type middleware func(http.Handler) http.Handler
+
+type Router struct {
+	middlewareChain [] middleware
+	mux map[string] http.Handler
+}
+
+func NewRouter() *Router{
+	return &Router{}
+}
+
+func (r *Router) Use(m middleware) {
+	r.middlewareChain = append(r.middlewareChain, m)
+}
+
+func (r *Router) Add(route string, h http.Handler) {
+	var mergedHandler = h
+
+	for i := len(r.middlewareChain) - 1; i >= 0; i-- {
+		mergedHandler = r.middlewareChain[i](mergedHandler)
+	}
+
+	r.mux[route] = mergedHandler
 }
