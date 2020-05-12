3d6bd49e989c82c7e9830fbe08e4508dada0b271
“t_xinlin@sina.com <Happy100>
2020-05-12 16:43:39 +0800

    Fix go lint

--- pkg/uuid/uuid_test.go
+++ pkg/uuid/uuid_test.go
@@ -1,1 +1,9 @@
 package uuid
+
+import "testing"
+
+func TestNewMD5(t *testing.T) {
+	uuid := New()
+	u := NewMD5(uuid, []byte("Hello"))
+	t.Log(u.String())
+}
--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -2,6 +2,7 @@
 set -e
 # Make the Coverage File
 echo "mode: atomic" > coverage.txt
+
 # Make Necessary directories needed by Test (Ideally it should get created automatically but Travis is not allowing to create it using os.MkdriAll)
 # I know this is insane but nothing can be done
 
